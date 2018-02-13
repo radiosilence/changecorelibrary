be2b75c9e09fcfdaf67abb00f4a0041e2a0a98eb
Matthew Thomas <matt@afternight.io>
2018-02-13 19:58:32 +1100

    Added core create

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"database/sql"
 	"strconv"
 	"strings"
+	"bitbucket.org/tokom_/linkcore"
+	"github.com/Afternight/Catch"
 )
 
 func CreateCodesFromSources(codes []core_sdk.IntegrationCode, switchboard []core_sdk.IntegrationCode, inverted bool) core_sdk.IntCodes {
@@ -78,4 +80,85 @@ func AddCorrelations(pk int64, newIDs []core_sdk.ExtID, db *sql.DB, tableName st
 	}
 
 	return nil
+}
+
+func CreateChangeCore(request linkcore.CreateCommonRequest,db *sql.DB, origin string)(linkcore.CreateCommonResponse){
+	//Create our response
+	resp := request.GetNewResponseObject()
+
+	//Create the log
+	log := new(Catch.Log)
+
+	//set fluids
+	setErr := resp.SetObjectValues(request.GetValues())
+
+	if setErr != nil {
+		log.AddNewFailureFromError(500,core_sdk.ProductDomain,setErr,true,request.GetCreateRectifier(origin))
+		resp.SetLog(*log)
+		return resp
+	}
+
+	//Create the object in the database
+	res, dbErr := request.InsertIntoDatabase(db)
+	if dbErr != nil {
+		log.AddNewFailureFromError(500,core_sdk.ProductDomain,dbErr,true,request.GetCreateRectifier(origin))
+		resp.SetLog(*log)
+		return resp
+	}
+	pkID, pkErr := res.LastInsertId()
+	if pkErr != nil {
+		log.AddNewFailureFromError(500,core_sdk.ProductDomain,pkErr,true,request.GetCreateRectifier(origin))
+		resp.SetLog(*log)
+		return resp
+	}
+
+	//Set tokoms now that they exist in the database
+	tempTokom := new(linkcore.Tokom)
+	tempUser := request.GetUser()
+	tempTokom.CID = tempUser.CID
+	tempTokom.CreatedBy = tempUser.UID
+	tempTokom.PrimaryKey = pkID
+	tempTokom.CreatedAt = "Not Implemented"
+
+	resp.SetObjectTokomValues(*tempTokom)
+
+	//Get switchboard codes
+	var stubbedSwitch []core_sdk.IntegrationCode //todo make this actually get from switchboard
+
+	//compare and reset codes
+	tempCodes := CreateCodesFromSources(request.GetCodes().Values,stubbedSwitch,request.GetCodes().Inverted)
+	request.SetCodes(tempCodes)
+
+	//create product in our integrations
+	dO, _, deltaErr := request.EnactDelta()
+
+
+	//check if there was an error in sending the delta request
+	if deltaErr != nil {
+		log.AddNewFailureFromError(500,core_sdk.ProductDomain,deltaErr,false,request.GetInstallRectifier(*tempTokom,origin))
+		resp.SetLog(*log)
+		//the responses object values have already been set
+		return resp
+	}
+
+	deltaCommonReq := dO.(*core_sdk.CommonDeltaResponse)
+	log.MergeLogs(deltaCommonReq.Log)
+
+	if deltaCommonReq.Log.Fatality { //check if there was a fatality on delta
+		resp.SetLog(*log)
+		return resp
+	}
+
+	//if we are here we assume delta enacted atleast partially and begin installing
+	corrErr := AddCorrelations(tempTokom.PrimaryKey, deltaCommonReq.IDs,db,request.GetTableName())
+
+	//A corr err in this case means a complete failure of correlation insertion
+	if corrErr != nil {
+		log.AddNewFailureFromError(500, core_sdk.ProductDomain,corrErr,false,request.GetLinkRectifier(*tempTokom,origin,deltaCommonReq.IDs))
+	} else {
+		resp.SetIDs(deltaCommonReq.IDs)
+	}
+
+	resp.SetLog(*log)
+	return resp
 }
