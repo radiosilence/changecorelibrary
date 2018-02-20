fa262a4781ebe3342c051ab96fe429b33191a7a4
Matthew Thomas <matt@afternight.io>
2018-02-20 11:03:29 +1100

    Added link change core

--- main.go
+++ main.go
@@ -348,3 +348,22 @@ func InstallChangeCore (request linkcore.InstallRequest, db *sql.DB, origin stri
 	resp.SetLog(*log)
 	return resp
 }
+
+func LinkChangeCore(request linkcore.LinkRequest, db *sql.DB, origin string) (linkcore.LinkResponse){
+	log := new(Catch.Log)
+	resp := request.ConstructNewResponseObject().(linkcore.LinkResponse)
+	corrErr := AddCorrelations(request.GetPrimaryKey(),request.GetIDs(),db,request.GetObjectHandle())
+
+	if corrErr != nil {
+		log.AddNewFailureFromError(500,origin,corrErr,true,request.GetLinkRectifier(origin))
+		resp.SetLog(*log)
+		return resp
+	}
+
+	setErr := resp.SetObjectFromPK(db,request.GetPrimaryKey())
+
+	if setErr != nil {
+		log.AddNewFailureFromError(500,origin,corrErr,false,request.GetLinkRectifier(origin))
+	}
+	return resp
+}
